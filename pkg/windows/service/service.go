5debacde14dc5942b25e459f64313f3e74918639
Ubuntu <azureuser@yuanlianglinux.0dssu1lutywubph3xrtc4jr3og.xx.internal.cloudapp.net>
2024-08-06 22:32:41 +0000

    Temp testing related change

--- pkg/windows/service/service.go
+++ pkg/windows/service/service.go
@@ -20,12 +20,9 @@ limitations under the License.
 package service
 
 import (
-	"os"
 	"syscall"
-	"time"
 	"unsafe"
 
-	"k8s.io/apiserver/pkg/server"
 	"k8s.io/klog/v2"
 
 	"golang.org/x/sys/windows"
@@ -148,37 +145,39 @@ Loop:
 				s <- c.CurrentStatus
 			case svc.Interrogate:
 				s <- c.CurrentStatus
-			case svc.Stop:
-				klog.Infof("Service stopping")
-				// We need to translate this request into a signal that can be handled by the signal handler
-				// handling shutdowns normally (currently apiserver/pkg/server/signal.go).
-				// If we do not do this, our main threads won't be notified of the upcoming shutdown.
-				// Since Windows services do not use any console, we cannot simply generate a CTRL_BREAK_EVENT
-				// but need a dedicated notification mechanism.
-				graceful := server.RequestShutdown()
+			// case svc.Stop:
+			// 	klog.Infof("Service stopping")
+			// 	// We need to translate this request into a signal that can be handled by the signal handler
+			// 	// handling shutdowns normally (currently apiserver/pkg/server/signal.go).
+			// 	// If we do not do this, our main threads won't be notified of the upcoming shutdown.
+			// 	// Since Windows services do not use any console, we cannot simply generate a CTRL_BREAK_EVENT
+			// 	// but need a dedicated notification mechanism.
+			// 	graceful := server.RequestShutdown()
 
-				// Free up the control handler and let us terminate as gracefully as possible.
-				// If that takes too long, the service controller will kill the remaining threads.
-				// As per https://docs.microsoft.com/en-us/windows/desktop/services/service-control-handler-function
-				s <- svc.Status{State: svc.StopPending}
+			// 	// Free up the control handler and let us terminate as gracefully as possible.
+			// 	// If that takes too long, the service controller will kill the remaining threads.
+			// 	// As per https://docs.microsoft.com/en-us/windows/desktop/services/service-control-handler-function
+			// 	s <- svc.Status{State: svc.StopPending}
 
-				// If we cannot exit gracefully, we really only can exit our process, so at least the
-				// service manager will think that we gracefully exited. At the time of writing this comment this is
-				// needed for applications that do not use signals (e.g. kube-proxy)
-				if !graceful {
-					go func() {
-						// Ensure the SCM was notified (The operation above (send to s) was received and communicated to the
-						// service control manager - so it doesn't look like the service crashes)
-						time.Sleep(1 * time.Second)
-						os.Exit(0)
-					}()
-				}
-				break Loop
-			case svc.PreShutdown:
+			// 	// If we cannot exit gracefully, we really only can exit our process, so at least the
+			// 	// service manager will think that we gracefully exited. At the time of writing this comment this is
+			// 	// needed for applications that do not use signals (e.g. kube-proxy)
+			// 	if !graceful {
+			// 		go func() {
+			// 			// Ensure the SCM was notified (The operation above (send to s) was received and communicated to the
+			// 			// service control manager - so it doesn't look like the service crashes)
+			// 			time.Sleep(1 * time.Second)
+			// 			os.Exit(0)
+			// 		}()
+			// 	}
+			// 	break Loop
+			case svc.Stop, svc.PreShutdown:
 				klog.Infof("Service pre-shutdown")
 				if h.shutdownhandler != nil {
 					h.shutdownhandler.ProcessShutdownEvent()
 				}
+				s <- svc.Status{State: svc.StopPending}
+
 				break Loop
 			}
 		}
