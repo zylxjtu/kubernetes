dd51dfd0109c8a5af9cffb6af1bc022b2fcee378
vinay kulkarni <vskibum@gmail.com>
2025-03-20 05:57:41 +0000

    Verify oom_score_adj for containers that have been restarted in pod resize e2e

--- pkg/kubelet/kubelet_pods.go
+++ pkg/kubelet/kubelet_pods.go
@@ -2115,7 +2115,7 @@ func (kl *Kubelet) convertToAPIContainerStatuses(pod *v1.Pod, podStatus *kubecon
 			if _, exists := resources.Requests[v1.ResourceMemory]; exists {
 				// Get memory requests from actuated resources
 				if actuatedResources, found := kl.allocationManager.GetActuatedResources(pod.UID, allocatedContainer.Name); found {
-					resources.Requests[v1.ResourceMemory] = actuatedResources.Requests.Memory().DeepCopy()
+					resources.Requests[v1.ResourceMemory] = *actuatedResources.Requests.Memory()
 				}
 			}
 		}
