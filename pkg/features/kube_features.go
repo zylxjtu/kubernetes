610e6a111e91b3b14c16a8cef2c189c6c091086a
Michael Aspinwall <maspinwall@google.com>
2025-11-13 00:06:30 +0000

    Remove CRD stored versions from status upon SVM migration

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -1968,6 +1968,7 @@ var defaultVersionedKubernetesFeatureGates = map[featuregate.Feature]featuregate
 
 	apiextensionsfeatures.CRDObservedGenerationTracking: {
 		{Version: version.MustParse("1.35"), Default: false, PreRelease: featuregate.Beta},
+		{Version: version.MustParse("1.36"), Default: true, PreRelease: featuregate.Beta},
 	},
 
 	apiextensionsfeatures.CRDValidationRatcheting: {
