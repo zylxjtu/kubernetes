dce9c35741b050575627674f76d9f80ef69c06d3
Yuan Wang <yuanwangyw@google.com>
2026-01-16 21:08:11 +0000

    Graduate feature to beta

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -1750,6 +1750,7 @@ var defaultVersionedKubernetesFeatureGates = map[featuregate.Feature]featuregate
 
 	RestartAllContainersOnContainerExits: {
 		{Version: version.MustParse("1.35"), Default: false, PreRelease: featuregate.Alpha},
+		{Version: version.MustParse("1.36"), Default: true, PreRelease: featuregate.Beta},
 	},
 
 	RotateKubeletServerCertificate: {
