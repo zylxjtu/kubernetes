7dc659a9495850003d7b667efe254e661d6be63a
Karthik Bhat <karthikkn1997@gmail.com>
2026-01-23 14:54:16 +0530

    Refactor NewTestContext to return Context instead of TContext

--- test/integration/scheduler_perf/executor_test.go
+++ test/integration/scheduler_perf/executor_test.go
@@ -213,7 +213,7 @@ func TestRunOp(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			_, tCtx := ktesting.NewTestContext(t)
+			tCtx := ktesting.Init(t)
 			client := fake.NewSimpleClientset()
 			tCtx = tCtx.WithClients(nil, nil, client, nil, nil)
 
@@ -618,7 +618,7 @@ func TestMetricThreshold(t *testing.T) {
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			_, tCtx := ktesting.NewTestContext(t)
+			tCtx := ktesting.Init(t)
 			var capturedErr error
 			capturingCtx, finalize := tCtx.WithError(&capturedErr)
 			defer finalize()
