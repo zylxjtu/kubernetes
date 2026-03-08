1cc57cfa3c5f42f9cc460ab187791ceb91032213
Jose Valdes <jvaldes@redhat.com>
2026-03-07 22:53:52 -0500

    feature: promote NodeLogQuery to GA and lock default

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -1705,6 +1705,7 @@ var defaultVersionedKubernetesFeatureGates = map[featuregate.Feature]featuregate
 	NodeLogQuery: {
 		{Version: version.MustParse("1.27"), Default: false, PreRelease: featuregate.Alpha},
 		{Version: version.MustParse("1.30"), Default: false, PreRelease: featuregate.Beta},
+		{Version: version.MustParse("1.36"), Default: true, PreRelease: featuregate.GA, LockToDefault: true}, // remove in 1.39, locked to default in 1.36
 	},
 
 	NodeSwap: {
