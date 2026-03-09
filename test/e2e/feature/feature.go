b5ee1c22759f24c7baea9b680d8d1730eb9cab46
Praveen M <m.praveen@ibm.com>
2026-03-09 14:59:50 +0530

    Update SnapshotMetadataService CRD API version from v1alpha1 to v1beta1
    Align e2e tests with upstream external-snapshot-metadata change
    (kubernetes-csi/external-snapshot-metadata#215) which promotes the
    SnapshotMetadataService API to v1beta1.
    
    Signed-off-by: Praveen M <m.praveen@ibm.com>

--- test/e2e/feature/feature.go
+++ test/e2e/feature/feature.go
@@ -423,7 +423,7 @@ var (
 	// Tests marked with this feature require:
 	// - A CSI driver that supports the snapshot metadata service (e.g., CSI hostpath driver with --enable-snapshot-metadata)
 	// - The external-snapshot-metadata sidecar deployed alongside the CSI driver
-	// - The SnapshotMetadataService CRD (cbt.storage.k8s.io/v1alpha1) installed
+	// - The SnapshotMetadataService CRD (cbt.storage.k8s.io/v1beta1) installed
 	// - A storage driver that implements the CapSnapshotMetadata capability
 	SnapshotMetadata = framework.WithFeature(framework.ValidFeatures.Add("SnapshotMetadata"))
 
