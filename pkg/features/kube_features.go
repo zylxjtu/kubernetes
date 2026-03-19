0f523bbf61a39f0fbaca4cb5196be1346ae27d0e
Davanum Srinivas <davanum@gmail.com>
2026-03-19 13:04:47 -0400

    Switch PLEGOnDemandRelist default to `false` for 1.36

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -1707,7 +1707,7 @@ var defaultVersionedKubernetesFeatureGates = map[featuregate.Feature]featuregate
 	},
 
 	PLEGOnDemandRelist: {
-		{Version: version.MustParse("1.36"), Default: true, PreRelease: featuregate.Beta},
+		{Version: version.MustParse("1.36"), Default: false, PreRelease: featuregate.Beta},
 	},
 
 	PersistentVolumeClaimUnusedSinceTime: {
